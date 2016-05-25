05a794083f5fb746d6a75180617b703006e3cad1
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2016-05-25 13:20:49 -0300

    Added Google map with iframe.
    - Disabled mouse wheel unless clicked on it.

--- assets/javascripts/googleMaps.js
+++
@@ -1,9 +0,0 @@
-var map;
-function initMap() {
-// Create a map object and specify the DOM element for display.
-	map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: -34.397, lng: 150.644},
-	  scrollwheel: false,
-	  zoom: 8
-	});
-}
