43e35ee598016890e63c1cde9b72927bb1a03af3
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2016-06-12 19:35:38 -0300

    Added menu highlighting on scroll.

--- js/mikroways.js
+++ js/mikroways.js
@@ -39,6 +39,7 @@ var isNavBarShown = false;
     });
 }(jQuery));
 
+// Smooth scrolling.
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -52,4 +53,43 @@ $(function() {
     }
   }
   });
+});
+
+/**
+ * This part handles the menu highlighting functionality.
+ */
+$(document).ready(function(){
+  var aChildren = $("nav div.main-menu li").children(); // find the a children of the list items
+  var aArray = []; // create the empty aArray
+  for (var i=0; i < aChildren.length; i++) {    
+    var aChild = aChildren[i];
+    var ahref = $(aChild).attr('href');
+    aArray.push(ahref);
+  } // this for loop fills the aArray with attribute href values
+
+  $(window).scroll(function(){
+    var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+    var windowHeight = $(window).height(); // get the height of the window
+    var docHeight = $(document).height();
+
+    for (var i=0; i < aArray.length; i++) {
+      var theID = aArray[i];
+      var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+      var divHeight = $(theID).height(); // get the height of the div in question
+      if (windowPos >= divPos - 1 && windowPos + 1 < (divPos + divHeight)) {
+        $("a[href='" + theID + "']").addClass("nav-active");
+      } else {
+        $("a[href='" + theID + "']").removeClass("nav-active");
+      }
+    }
+
+    if(windowPos + windowHeight == docHeight) {
+      if (!$("nav li:last-child a").hasClass("nav-active")) {
+        var navActiveCurrent = $(".nav-active").attr("href");
+        $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+        $("nav li:last-child a").addClass("nav-active");
+      }
+    }
+  });
+
 });
